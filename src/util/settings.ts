987249924d32b12e209978714eecf4eab3385668
CodenNerd <aatanda.dammy@gmail.com>
2022-01-13 18:20:01 +0100

    make env vars of Trust creds

--- src/util/settings.ts
+++ src/util/settings.ts
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 export const settings : any = {
     MOBILE_MONEY_MAX: 500000,
     CASH_PICKUP_MAX: 20000,
     BANK_TRANSFER_MAX: 20000,
     TRANSFER_TIME_LIMIT_IN_SECONDS: 60 * 60 * 24, // 24 hrs
 
-    TRUST_PAYMENT_SITE_REFERENCE: 'test_sukatebezebohltd89498',
-    TRUST_NOTIFICATION_WEBHOOK_URL: 'https://api-uat.sbremit.co.uk/trust/payment/notification',
-    TRUST_SUCCESSFUL_REDIRECT_URL: 'https://sbremit.netlify.app/transfer-complete?t='
+    TRUST_PAYMENT_SITE_REFERENCE: process.env.REACT_APP_TRUST_PAYMENT_SITE_REFERENCE,
+    TRUST_NOTIFICATION_WEBHOOK_URL: process.env.REACT_APP_TRUST_NOTIFICATION_WEBHOOK_URL,
+    TRUST_SUCCESSFUL_REDIRECT_URL: process.env.REACT_APP_TRUST_SUCCESSFUL_REDIRECT_URL
+
 }
