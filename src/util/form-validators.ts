1df724ed15907ff9d997c8fd075b73eeb8b506b2
sh3riff <contactme@sh3riff.me>
2022-03-17 08:18:08 +0100

    Implmented Phone number Login & signup

--- src/util/form-validators.ts
+++ src/util/form-validators.ts
@@ -4,12 +4,14 @@ export const SignUpValidator = Yup.object().shape({
     firstName: Yup.string().min(2, "Too short").max(30, "Too long").required("Required"),
     lastName: Yup.string().min(2, "Too short").max(30, "Too long").required("Required"),
     location_country: Yup.string().min(2, "Too short").max(2, "Too long").required("Required"),
-    username: Yup.string().required("Required").email("Enter valid email"),
+    // username: Yup.string().required("Required").email("Enter valid email"),
+    username: Yup.string().required("Required"),
     password: Yup.string().min(8, "Minimum password length is 8 characters").max(20, "Max password length is 20 characters").required("Required"),
 });
 
 export const SignInValidator = Yup.object().shape({
-    username: Yup.string().required("Required").email("Enter valid email"),
+    // username: Yup.string().required("Required").email("Enter valid email"),
+    username: Yup.string().required("Required"),
     password: Yup.string().min(8, "Minimum password length is 8 characters").max(20, "Max password length is 20 characters").required("Required")
 });
 
