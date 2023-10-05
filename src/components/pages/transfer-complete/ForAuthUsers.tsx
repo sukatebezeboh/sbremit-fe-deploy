9c95aa3fe267fea06d805f01b3319c74e2261644
Mjas360 <amosasulyman360@gmail.com>
2023-10-05 08:52:06 +0100

    Refactor forAuthUsers.tsx

--- src/components/pages/transfer-complete/ForAuthUsers.tsx
+++ src/components/pages/transfer-complete/ForAuthUsers.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from "react";
+import PageNavButton from "components/modules/parts/PageNavButton";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import NavBar from "../../modules/navbar/NavBar";
-import TransferDetailsBox from "../../modules/transfer-details-box/TransferDetailsBox";
-import ProgressBar from "../../modules/progress-bar/ProgressBar";
-import SuccessIcon from "../../modules/success-icon/SuccessIcon";
-import { getQueryParam, isCurrencyPairDowntimeUp } from "../../../util/util";
-import Body from "./TransferComplete.css";
 import {
   getPaymentStatus,
   getSpreads,
   getTransactionDetails,
 } from "redux/actions/actions";
-import { useSelector } from "react-redux";
 import { paths } from "util/paths";
-import PageNavButton from "components/modules/parts/PageNavButton";
+import { getQueryParam, isCurrencyPairDowntimeUp } from "../../../util/util";
+import NavBar from "../../modules/navbar/NavBar";
+import ProgressBar from "../../modules/progress-bar/ProgressBar";
+import SuccessIcon from "../../modules/success-icon/SuccessIcon";
+import TransferDetailsBox from "../../modules/transfer-details-box/TransferDetailsBox";
+import Body from "./TransferComplete.css";
+
+interface PaymentStatusProps {
+  status: string;
+  code: any;
+  message: string;
+}
 
 const ForAuthUsers = () => {
   const history = useHistory();
@@ -22,7 +28,8 @@ const ForAuthUsers = () => {
   const transfer = useSelector(
     (state: any) => state.transfer.transactionDetails
   );
-  const countries = useSelector((state: any) => state.appValues.countries);
+
+  const [paymentSatus, setPaymentStatus] = useState<PaymentStatusProps>();
 
   useEffect(() => {
     getSpreads();
@@ -34,13 +41,10 @@ const ForAuthUsers = () => {
   useEffect(() => {
     if (transfer !== undefined) {
       const checkoutId = transfer?.meta?.axcess_checkout_id;
-      getPaymentStatus(checkoutId);
+
+      getPaymentStatus(checkoutId, setPaymentStatus);
     }
   }, [transfer]);
-
-  const getCountryFullNameByCountryKey = (key: string): string => {
-    return countries[key] || key;
-  };
 
   return (
     <Body>
@@ -49,42 +53,20 @@ const ForAuthUsers = () => {
       <div className="page-content">
         <div className="box-container details">
           <div className="completed-box">
-            <div className="success-icon">
-              <SuccessIcon />
-            </div>
-            <div className="completed-head">Transfer created!</div>
-            <div className="completed-body">
-              Your transfer process will continue when SBremit receives the
-              payment from your bank. You will receive an email confirmation
-              {
-                // transfer?.meta?.paymentGatewayUsed === 'TRUELAYER' &&
-                <p className="green-txt">
-                  Please note, it may take up to 3 minutes for the status of
-                  your transaction to be updated.
-                </p>
-              }
-              {isCurrencyPairDowntimeUp(
-                transfer?.originCurrency,
-                transfer?.destinationCurrency
-              ) && (
-                <p>
-                  We are experiencing some delays with payments to{" "}
-                  {getCountryFullNameByCountryKey(
-                    transfer?.meta?.destinationCountryCode
-                  )}{" "}
-                  caused by temporary downtime. We are working to resolve this
-                  quickly and you will be notified once the system is up and
-                  running, we apologise for any inconvenience this may have
-                  caused.
-                </p>
-              )}
-            </div>
-
-            <div className="download">
-              {/* <div>
-                                <img src={asset('icons', 'download-file.svg')} alt="download"/><span>Download receipt</span>
-                            </div> */}
-            </div>
+            {/* {paymentSatus?.status !== null && paymentSatus?.status === "error" ? (
+              <div>
+                <div className="error-icon">
+                  <MdOutlineError size={100} color="#cf0921" />
+                </div>
+                <div className="completed-head">Payment error!</div>
+                <div className="error-body">
+                  {paymentSatus?.message} Payment processed with suspicious card
+                  details: Country Mismatch
+                </div>
+              </div>
+            ) : ( */}
+            <TransferCreated />
+            {/* )} */}
             <div className="back">
               <PageNavButton
                 onClick={() => history.push(paths.DASHBOARD)}
@@ -101,3 +83,50 @@ const ForAuthUsers = () => {
 };
 
 export default ForAuthUsers;
+
+const TransferCreated = (transfer: any) => {
+  const countries = useSelector((state: any) => state.appValues.countries);
+
+  const getCountryFullNameByCountryKey = (key: string): string => {
+    return countries[key] || key;
+  };
+  return (
+    <>
+      <div className="success-icon">
+        <SuccessIcon />
+      </div>
+      <div className="completed-head">Transfer created!</div>
+      <div className="completed-body">
+        Your transfer process will continue when SBremit receives the payment
+        from your bank. You will receive an email confirmation
+        {
+          // transfer?.meta?.paymentGatewayUsed === 'TRUELAYER' &&
+          <p className="green-txt">
+            Please note, it may take up to 3 minutes for the status of your
+            transaction to be updated.
+          </p>
+        }
+        {isCurrencyPairDowntimeUp(
+          transfer?.originCurrency,
+          transfer?.destinationCurrency
+        ) && (
+          <p>
+            We are experiencing some delays with payments to{" "}
+            {getCountryFullNameByCountryKey(
+              transfer?.meta?.destinationCountryCode
+            )}{" "}
+            caused by temporary downtime. We are working to resolve this quickly
+            and you will be notified once the system is up and running, we
+            apologise for any inconvenience this may have caused.
+          </p>
+        )}
+      </div>
+
+      <div className="download">
+        {/* <div>
+                    <img src={asset('icons', 'download-file.svg')} alt="download"/><span>Download receipt</span>
+                </div> */}
+      </div>
+    </>
+  );
+};
