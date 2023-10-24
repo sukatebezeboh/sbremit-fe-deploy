8c2df491469ab53ecb3d3fd9e4f1174ebae2bff3
Mjas360 <amosasulyman360@gmail.com>
2023-10-24 07:35:58 +0100

    Fix: Update verification CTA, naviagate to dashboard

--- src/components/pages/verification/VerificationComponent.tsx
+++ src/components/pages/verification/VerificationComponent.tsx
@@ -5,28 +5,26 @@ import {
   refreshUserDetails,
   userVerificationAction,
 } from "../../../redux/actions/actions";
-import {
-  constants,
-} from "../../../util/constants";
+import { constants } from "../../../util/constants";
 import { userVerificationValidator } from "../../../util/form-validators";
 import PageHeading from "../../modules/page-heading/PageHeading";
 
-import Body from './Verification.css';
+import Body from "./Verification.css";
 import VerificationForm from "./VerificationForm";
 import { ComplyCubeVerification } from "./ComplyCubeVerification";
-
+import { useHistory } from "react-router-dom";
 
 // INVALID VALID PENDING FAILED
 
 export const VerificationComponent = () => {
-  const [selectedCountry, setSelectedCountry] =
-    useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const history = useHistory();
 
   const [method, setMethod] = useState("");
   const user = useSelector((state: any) => state.auth.user);
 
-  const transferId = new URLSearchParams(window.location.search).get('t');
-  console.log("transferId", transferId)
+  const transferId = new URLSearchParams(window.location.search).get("t");
+  console.log("transferId", transferId);
 
   const initialValues: any = {
     // phoneCode: "+01",
@@ -36,33 +34,52 @@ export const VerificationComponent = () => {
   };
 
   useEffect(() => {
-    refreshUserDetails()
-  }, [])
+    refreshUserDetails();
+  }, []);
 
-    useEffect(() => {
-      setSelectedCountry(initialValues.location_country)
-    }, [initialValues.location_country])
+  useEffect(() => {
+    setSelectedCountry(initialValues.location_country);
+  }, [initialValues.location_country]);
 
-  const isFormVerified = Boolean(user?.meta?.verified)
+  const isFormVerified = Boolean(user?.meta?.verified); //user?.meta?.verified !== "1" ? false : true;
+
+  const verifyUser = async (values: any) => {
+    try {
+      await userVerificationAction(values, () => {});
+      history.push("dashboard");
+    } catch (error) {}
+  };
 
   return (
-    <div style={{padding: "10px", minHeight: "100vh"}}>
-      {!isFormVerified ? (<Body>
+    <div style={{ padding: "10px", minHeight: "100vh" }}>
+      {!isFormVerified ? (
+        <Body>
           <div className="page-content">
-          <Formik
-            initialValues={{ ...initialValues }}
-            validationSchema={userVerificationValidator}
-            onSubmit={(values) => {
-              userVerificationAction(values, () => {})
-            }}
-          >
-            {({ errors, touched, values }: any) => (
-            <VerificationForm {...{ errors, touched, values, selectedCountry, setSelectedCountry }}/>
-            )}
-          </Formik>
+            <Formik
+              initialValues={{ ...initialValues }}
+              validationSchema={userVerificationValidator}
+              onSubmit={(values) => {
+                verifyUser(values);
+              }}
+            >
+              {({ errors, touched, values }: any) => (
+                <VerificationForm
+                  {...{
+                    errors,
+                    touched,
+                    values,
+                    selectedCountry,
+                    setSelectedCountry,
+                  }}
+                />
+              )}
+            </Formik>
           </div>
-      </Body>) : <p>✅ Verification form completed</p>}
-      
+        </Body>
+      ) : (
+        <p>✅ Verification form completed</p>
+      )}
+
       <ComplyCubeVerification />
     </div>
   );
