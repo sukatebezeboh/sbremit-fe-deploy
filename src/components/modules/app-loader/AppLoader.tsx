bca4246172e9af2ea76ec5631a00bca2275d7f90
Mjas360 <amosasulyman360@gmail.com>
2023-11-06 07:04:05 +0100

    Modify Apploader css

--- src/components/modules/app-loader/AppLoader.tsx
+++ src/components/modules/app-loader/AppLoader.tsx
@@ -1,40 +1,50 @@
-import React from 'react'
-import { asset } from '../../../util/util';
-import styled from 'styled-components';
+import React from "react";
+import { asset } from "../../../util/util";
+import styled from "styled-components";
 
 const Div = styled.div`
-    width: 100%;
-    height: 150vh;
-    background-color: #1a191999;
-    position: fixed;
-    z-index: 30;
-    top: 0px;
-    left: 0px;
-    img {
-        display: block;
-        margin: 45vh auto;
-        width: 50px;
-        height: 50px;
-    }
-    .text {
-        display: block;
-        font-size: 32px;
-        text-align: center;
-        color: white;
-        margin-top: -10%;
-        font-weight: 800;
-    }
-`
+  width: 100%;
+  height: 100vh;
+  background-color: #1a191967;
+  position: absolute;
+  z-index: 30;
+  top: 0px;
+  left: 0px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 18px;
+
+  img {
+    display: block;
+    //margin: 45vh auto;
+    width: 50px;
+    height: 50px;
+  }
+  .text {
+    display: block;
+    font-size: 18px;
+    text-align: center;
+    line-height: 150%;
+    color: #eae6e6;
+    //margin-top: -10%;
+    font-weight: 800;
+  }
+`;
 
 const AppLoader = (props: any) => {
-    const {show} = props;
-    return (
-        show ? <Div>
-            <img src={asset('icons', 'rolling-loader.svg')} alt="page-loader"/>
+  const { show } = props;
+  return show ? (
+    <Div>
+      <img src={asset("icons", "rolling-loader.svg")} alt="page-loader" />
 
-            <div className="text">{show}</div>
-        </Div> : <></>
-    )
-}
+      <div className="text">{show}</div>
+    </Div>
+  ) : (
+    <></>
+  );
+};
 
-export default AppLoader
+export default AppLoader;
