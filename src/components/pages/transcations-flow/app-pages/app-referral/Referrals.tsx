bb33c09c16c16f56efa848644ed11d9afc1f9996
Mjas360 <amosasulyman360@gmail.com>
2023-11-08 02:49:41 +0100

    Add and redesign referral page

---
+++ src/components/pages/transcations-flow/app-pages/app-referral/Referrals.tsx
@@ -0,0 +1,270 @@
+import { TwitterOutlined, WhatsAppOutlined } from "@ant-design/icons";
+import {
+  Avatar,
+  Button,
+  Card,
+  Divider,
+  Progress,
+  Space,
+  Statistic,
+} from "antd";
+import _env from "env";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getUserReferrals } from "redux/actions/actions";
+import { paths } from "util/paths";
+import {
+  copyToClipBoard,
+  getPercentage,
+  getUserDefaultCurrency,
+  getValueFromArray,
+} from "../../../../../util/util"; //"util/util";
+import { PageTitileAndDescription } from "../../utils/ReusablePageContent";
+import {
+  generateAlphabetColor,
+  getFirstLetter,
+} from "../../utils/reuseableUtils";
+import { Title } from "../app-dashboard/DashboardSyles";
+import {
+  HeaderStyles,
+  InsightStyles,
+  LinkContainerStyles,
+  LinkStyles,
+  ReferralContainerStyles,
+  ReferralContentStyles,
+  UsageStyles,
+  PromoUserNameStyles,
+} from "./ReferralsStyles";
+const { Meta } = Card;
+
+function isNotAwaiting(user: any, referralSettings: any) {
+  return (
+    Number(
+      getPercentage(
+        user?.[`cummulativeTransfer`],
+        referralSettings?.data?.referralActivationAmount
+      )
+    ) >= 100
+  );
+}
+
+export default function Referrals() {
+  const user = useSelector((state: any) => state.auth.user);
+  const appValues = useSelector((state: any) => state.appValues);
+  const referralSettings = getValueFromArray(
+    "settings",
+    "name",
+    appValues?.values?.data || []
+  );
+
+  const [referralDetails, setReferralDetails] = useState({
+    count: 0,
+    referredUsers: [],
+  });
+
+  useEffect(() => {
+    getUserReferrals(setReferralDetails);
+  }, []);
+
+  const getShareReferralText = (extraText = "") => {
+    return encodeURIComponent(
+      `SB REMIT is a cheap, fast and secure way of sending money to Africa. 
+		It even gets better - you can earn ${getUserDefaultCurrency(
+      user,
+      appValues,
+      true
+    )}${
+        referralSettings?.data?.referredUserDiscountValue
+      } when you sign-up with my referral link and make a successful transfer.
+
+		${extraText}
+		`
+    );
+  };
+
+  const defaultCurrency = getUserDefaultCurrency(user, appValues);
+  const getAccruedBonus = (users: any) => {
+    const filteredUsers = users?.filter(
+      (user: any) =>
+        user.useStatus === "Active" && isNotAwaiting(user, referralSettings)
+    );
+
+    return (
+      filteredUsers?.length * referralSettings?.data?.referrerDiscountValue
+    );
+  };
+
+  return (
+    <ReferralContainerStyles>
+      <PageTitileAndDescription
+        title="My Referral Page"
+        description="Refer a friend and earn £10!😛"
+      />
+      <ReferralContentStyles>
+        <HeaderStyles>
+          <div className="section_1">
+            <span>Referral code</span>
+            <p>{user?.referral?.code}</p>
+            <p>Share this on:</p>
+          </div>
+          <Space>
+            <a
+              rel="noreferrer"
+              href={`https://api.whatsapp.com/send?text=${getShareReferralText(
+                _env.APP_HOST +
+                  paths.SIGN_UP +
+                  "?referral=" +
+                  user?.referral?.code
+              )}`}
+              target="_blank"
+            >
+              <Avatar
+                icon={<WhatsAppOutlined rev={undefined} />}
+                style={{ background: "#25D366" }}
+              />
+            </a>
+            <a
+              rel="noreferrer"
+              href={`https://twitter.com/intent/tweet?url=${_env.APP_HOST}${
+                paths.SIGN_UP
+              }?referral=${
+                user?.referral?.code
+              }&text=${getShareReferralText()}&via=sbremit&hashtags=SBRemit`}
+              target="_blank"
+            >
+              <Avatar
+                icon={<TwitterOutlined rev={undefined} />}
+                style={{ background: "#1DA1F2" }}
+              />
+            </a>
+          </Space>
+        </HeaderStyles>
+        <Divider style={{ margin: 0 }}>or</Divider>
+        <LinkContainerStyles>
+          <LinkStyles
+            href={`${_env.APP_HOST}${paths.SIGN_UP}?referral=${user?.referral?.code}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {_env.APP_HOST}
+            {paths.SIGN_UP}?referral={user?.referral?.code}
+          </LinkStyles>
+          <Button
+            onClick={() =>
+              copyToClipBoard(
+                `${_env.APP_HOST}${paths.SIGN_UP}?referral=${user?.referral?.code}`
+              )
+            }
+          >
+            Copy link
+          </Button>
+        </LinkContainerStyles>
+      </ReferralContentStyles>
+
+      <Insight
+        count={referralDetails?.count}
+        accruedBonus={getAccruedBonus(referralDetails?.referredUsers)}
+        defaultCurrency={defaultCurrency}
+      />
+
+      <Usage
+        referralDetails={referralDetails}
+        referralSettings={referralSettings}
+      />
+    </ReferralContainerStyles>
+  );
+}
+
+const Insight = ({
+  accruedBonus,
+  defaultCurrency,
+  count,
+}: {
+  accruedBonus: any;
+  defaultCurrency: string;
+  count: number;
+}) => {
+  return (
+    <InsightStyles>
+      <Title>Insight</Title>
+      <div className="_insights">
+        <Card className="child">
+          <Statistic
+            title="Total users referred"
+            value={count}
+            precision={0}
+            valueStyle={{ color: "#d0cd23" }}
+          />
+        </Card>
+        <Card className="child">
+          <Statistic
+            title="Accrued bonus"
+            value={accruedBonus}
+            precision={0}
+            valueStyle={{ color: "#18a65f" }}
+            suffix={defaultCurrency}
+          />
+        </Card>
+      </div>
+    </InsightStyles>
+  );
+};
+
+const Usage = ({
+  referralDetails,
+  referralSettings,
+}: {
+  referralDetails: any;
+  referralSettings: any;
+}) => {
+  return (
+    <UsageStyles>
+      <Title>Usage</Title>
+      <div className="_uasge">
+        {referralDetails?.referredUsers?.map((user: any, index: number) => {
+          const referalCodeSTyles = {
+            width: "100%",
+            height: 80,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "8px 8px 0px 0px",
+            background: generateAlphabetColor(
+              getFirstLetter(user?.firstName) || ""
+            ),
+          };
+          let percentage = Math.min(
+            Number(
+              getPercentage(
+                user?.cummulativeTransfer,
+                referralSettings?.data?.referralActivationAmount
+              )
+            ),
+            100
+          );
+
+          return (
+            <Card
+              //hoverable
+              key={index + user.firstName}
+              cover={
+                <div style={referalCodeSTyles}>
+                  <PromoUserNameStyles>
+                    {user.firstName} {user.lastName}
+                  </PromoUserNameStyles>
+                </div>
+              }
+            >
+              <Meta
+                title="Awaiting activation"
+                description={
+                  <Progress percent={percentage} size="small" status="active" />
+                }
+              />
+            </Card>
+          );
+        })}
+      </div>
+    </UsageStyles>
+  );
+};
---
+++ src/components/pages/transcations-flow/app-pages/app-referral/ReferralsStyles.ts
@@ -0,0 +1,180 @@
+import styled from "styled-components";
+import {
+  Breakpoint,
+  Colors,
+  ResponsiveMarginTopForPagesWithSteps,
+} from "../../utils/stylesVariables";
+
+// export const ReferralContainerStyles = styled.div`
+//   box-sizing: border-box;
+//   margin: 32px 0;
+
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   flex-direction: column;
+//   gap: 32px;
+//   @media (max-width: ${Breakpoint.xl}) {
+//     margin: 24px 0;
+//     gap: 20;
+//   }
+//   @media (max-width: ${Breakpoint.sm}) {
+//     gap: 16px;
+//   }
+// `;
+
+export const ReferralContainerStyles = styled(
+  ResponsiveMarginTopForPagesWithSteps
+)`
+  box-sizing: border-box;
+  margin: 32px 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 32px;
+  @media (max-width: ${Breakpoint.xl}) {
+    margin: 24px 0;
+    gap: 20;
+  }
+  @media (max-width: ${Breakpoint.sm}) {
+    gap: 20px;
+  }
+`;
+
+export const ReferralContentStyles = styled.div`
+  display: flex;
+  width: 800px;
+  padding: 24px 24px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 16px;
+  border-radius: 14px;
+  border: 1px solid ${Colors.borderColor};
+  background: ${Colors.bgwhite};
+  box-sizing: border-box;
+
+  @media (max-width: ${Breakpoint.xl}) {
+    width: 64vw;
+    padding: 20px;
+    flex-direction: column;
+    gap: 14px;
+  }
+
+  @media (max-width: ${Breakpoint.md}) {
+    width: 85vw;
+    padding: 24px;
+    border-radius: 10px;
+  }
+`;
+
+export const HeaderStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 8px;
+
+  .section_1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    span {
+      font-size: 12px;
+      color: ${Colors.textColor2};
+    }
+
+    p {
+      margin: 0;
+      font-weight: 600;
+      font-size: 24px;
+      color: ${Colors.sbGreen};
+    }
+
+    span:last-child {
+      font-size: 14px;
+    }
+    p:last-child {
+      font-size: 14px;
+      font-weight: 400;
+      margin-top: 8px;
+      color: ${Colors.textColor};
+    }
+  }
+`;
+
+export const LinkContainerStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+export const LinkStyles = styled.a`
+  color: ${Colors.sbGreen};
+  text-decoration: underline;
+  font-size: 14px;
+  margin: 0;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const InsightStyles = styled(ReferralContentStyles)`
+  display: flex;
+  justify-content: flex-start;
+  gap: 24px;
+  align-items: center;
+  ._insights {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    gap: 24px;
+    @media (max-width: ${Breakpoint.md}) {
+      gap: 16px;
+    }
+    @media (max-width: ${Breakpoint.sm}) {
+      flex-direction: column;
+    }
+    .child {
+      flex: 1;
+    }
+  }
+`;
+
+export const UsageStyles = styled(ReferralContentStyles)`
+  gap: 24px;
+  ._uasge {
+    flex-wrap: wrap;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+    @media (max-width: ${Breakpoint.sm}) {
+      flex-direction: column;
+      width: 100%;
+      gap: 24;
+    }
+  }
+`;
+
+export const PromoUserNameStyles = styled.span`
+  width: 120px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  color: "white";
+  @media (max-width: ${Breakpoint.sm}) {
+    width: 200px;
+  }
+`;
