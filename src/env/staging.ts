a234c4240007a4633fe04adc50b4631170f59910
mjas360 <amosasulyman360@gmail.com>
2023-11-23 10:01:42 +0100

    update environment setup

--- src/env/staging.ts
+++ src/env/staging.ts
@@ -1,8 +1,8 @@
 const config = {
-  //API_HOST: '/api',
+  // API_HOST: 'http://localhost:80',
+  API_HOST: "/api",
   API_PROXY: "http://api-uat.sbremit.co.uk",
-  APP_HOST: "https://sbremit.netlify.app",
-  API_HOST: "https://api-uat.sbremit.co.uk",
+  APP_HOST: "https://sbremit-auth-redesign.onrender.com",
 };
 
 export default config;
