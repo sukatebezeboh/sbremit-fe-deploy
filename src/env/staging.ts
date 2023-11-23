74975db048911fa8dac2e7404ca000f4cd67319a
mjas360 <amosasulyman360@gmail.com>
2023-11-23 10:12:33 +0100

    update stagging .ts

--- src/env/staging.ts
+++ src/env/staging.ts
@@ -2,7 +2,7 @@ const config = {
   // API_HOST: 'http://localhost:80',
   API_HOST: "/api",
   API_PROXY: "http://api-uat.sbremit.co.uk",
-  APP_HOST: "https://sbremit-auth-redesign.onrender.com",
+  APP_HOST: "https://api-uat.sbremit.co.uk",
 };
 
 export default config;
