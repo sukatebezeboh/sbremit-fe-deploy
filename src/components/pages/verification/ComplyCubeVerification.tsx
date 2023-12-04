4fabae16f97fa03c56284f6b7fc33949d5b0d92e
mjas360 <amosasulyman360@gmail.com>
2023-12-04 16:00:41 +0100

    update checkToShowVerificationForm and checkIdVerificationStatus

--- src/components/pages/verification/ComplyCubeVerification.tsx
+++ src/components/pages/verification/ComplyCubeVerification.tsx
@@ -54,8 +54,10 @@ export const ComplyCubeVerification = ({
     (documentVerification && documentVerification.status === "PENDING") ||
     undefined;
 
-  const verificationCompleted =
-    !invalidIdVerification || !invalidDocumentVerification;
+  // const verificationCompleted =
+  //   !invalidIdVerification || !invalidDocumentVerification;
+
+  const verificationCompleted = Boolean(user?.meta?.verified);
 
   const stages: any = [
     {
