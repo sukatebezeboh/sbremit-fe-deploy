60e9ea73660fa79ca6502068ce626a18f5c233de
mjas360 <amosasulyman360@gmail.com>
2023-12-06 17:36:47 +0100

    update http to https

--- src/env/staging.ts
+++ src/env/staging.ts
@@ -1,7 +1,7 @@
 const config = {
-  API_HOST: "/api",
+  API_HOST: "https://api-uat.sbremit.co.uk",
   API_PROXY: "http://api-uat.sbremit.co.uk",
-  APP_HOST: "https://sbremit-fe12d.web.app/",
+  APP_HOST: "https://sbremit-auth-redesign.onrender.com",
 };
 
 export default config;
