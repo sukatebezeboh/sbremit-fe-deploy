2d997ad41afc3a792005632c9f22a744514d7679
mjas360 <amosasulyman360@gmail.com>
2024-05-16 16:10:11 +0100

    update payin min value

--- src/components/pages/transcations-flow/app-pages/app-getQuote/GetQuoteHelper.ts
+++ src/components/pages/transcations-flow/app-pages/app-getQuote/GetQuoteHelper.ts
@@ -107,9 +107,9 @@ export const isWithinPaymentLimit = (transfer: any) => {
   if (payinActualValue === 0) {
     return "";
   }
-  if (payinActualValue < 5) {
+  if (payinActualValue < 0.25) {
     //5
-    return `You can't send less than 5 ${payinCurrency}`;
+    return `You can't send less than 0.25 ${payinCurrency}`;
   }
   if (transferMethod === "mobile_money" && payoutActualValue > 500000) {
     return `The maximum transferrable amount inclusive of Mobile Operator Transfer Fees for mobile money is 500,000 ${payoutCurrency}`;
