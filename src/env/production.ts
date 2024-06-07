a74005f071562737eb96d7ee93c4190c813fd7ca
mjas360 <amosasulyman360@gmail.com>
2024-06-07 16:12:36 +0100

    fix: update Prod API_HOST to "https://api.sbremit.com"

--- src/env/production.ts
+++ src/env/production.ts
@@ -1,7 +1,8 @@
 const config = {
-    X_SERVICE_PROVIDER: "sbremit-web-prod",
-    API_HOST: 'https://sbremit.com:9090',
-    APP_HOST: 'https://sbremit.com'
-}
+  X_SERVICE_PROVIDER: "sbremit-web-prod",
+  // API_HOST: 'https://sbremit.com:9090',
+  API_HOST: "https://api.sbremit.com",
+  APP_HOST: "https://sbremit.com",
+};
 
 export default config;
